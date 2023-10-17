5e25fd3d95df03d1ed7c882f040d81ae24cd9d0e
opas-keat <opas.keat@gmail.com>
2023-10-17 21:58:17 +0700

    add search

--- lib/app/data/response/customer_service_response.dart
+++ lib/app/data/response/customer_service_response.dart
@@ -16,15 +16,15 @@ class CustomerServiceResponse {
     if (json['data'] != null) {
       data = <CustomerData>[];
       json['data'].forEach((v) {
-        data!.add(new CustomerData.fromJson(v));
+        data!.add(CustomerData.fromJson(v));
       });
     }
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['code'] = this.code;
-    data['message'] = this.message;
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['code'] = code;
+    data['message'] = message;
     if (this.data != null) {
       data['data'] = this.data!.map((v) => v.toJson()).toList();
     }
@@ -43,20 +43,19 @@ class CustomerData {
   String? customerLicensePlate;
   String? customerEmail;
   String? customerMile;
-  Null? products;
 
-  CustomerData(
-      {this.id,
-      this.warrantyNo,
-      this.warrantyDate,
-      this.dealerCode,
-      this.dealerName,
-      this.customerName,
-      this.customerPhone,
-      this.customerLicensePlate,
-      this.customerEmail,
-      this.customerMile,
-      this.products});
+  CustomerData({
+    this.id,
+    this.warrantyNo,
+    this.warrantyDate,
+    this.dealerCode,
+    this.dealerName,
+    this.customerName,
+    this.customerPhone,
+    this.customerLicensePlate,
+    this.customerEmail,
+    this.customerMile,
+  });
 
   CustomerData.fromJson(Map<String, dynamic> json) {
     id = json['id'];
@@ -69,22 +68,20 @@ class CustomerData {
     customerLicensePlate = json['customer_license_plate'];
     customerEmail = json['customer_email'];
     customerMile = json['customer_mile'];
-    products = json['products'];
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['warranty_no'] = this.warrantyNo;
-    data['warranty_date'] = this.warrantyDate;
-    data['dealer_code'] = this.dealerCode;
-    data['dealer_name'] = this.dealerName;
-    data['customer_name'] = this.customerName;
-    data['customer_phone'] = this.customerPhone;
-    data['customer_license_plate'] = this.customerLicensePlate;
-    data['customer_email'] = this.customerEmail;
-    data['customer_mile'] = this.customerMile;
-    data['products'] = this.products;
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['warranty_no'] = warrantyNo;
+    data['warranty_date'] = warrantyDate;
+    data['dealer_code'] = dealerCode;
+    data['dealer_name'] = dealerName;
+    data['customer_name'] = customerName;
+    data['customer_phone'] = customerPhone;
+    data['customer_license_plate'] = customerLicensePlate;
+    data['customer_email'] = customerEmail;
+    data['customer_mile'] = customerMile;
     return data;
   }
 }
